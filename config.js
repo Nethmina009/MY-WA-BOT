b3e56e8acb5fa45502cb0edf4ed3a0e66829df9d
Nethmina009 <168743416+Nethmina009@users.noreply.github.com>
2024-11-11 22:42:38 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,6 +5,6 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "TUMDnSAJ#1J2FV3mMLZhfuvAUrJqgvynhekifx7WHDK9bEpSkyeg",
+SESSION_ID: process.env.SESSION_ID || "PB0RnKpS#NjUaLMfXLjj_u1-2ute6ujemPb4e3tNbA0RX9qfcxYw",
 MONGODB: process.env.MONGODB || "mongodb+srv://kulathungaasitha319:yjHB0DvFfStNfwPS@cluster0.3oijd.mongodb.net",
 };
